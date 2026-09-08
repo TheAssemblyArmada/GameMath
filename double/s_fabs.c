66bd88b45b37ee0be824f45919d965887378e0eb
OmniBlade <nadia_xy@hotmail.com>
2026-09-08 09:28:52 +0100

    Implements fabs with SSE intrinsics.

--- double/s_fabs.c
+++ double/s_fabs.c
@@ -20,8 +20,20 @@
 double
 gm_fabs(double x)
 {
+#if defined USE_SSE && (defined __amd64__ || defined _M_AMD64)
+	// Testing shows this only improves performance on x86_64 builds
+	double ret;
+	const union {
+		unsigned long long __ui;
+		double __uf;
+	} sign_mask = { 0x7FFFFFFFFFFFFFFF };
+
+	_mm_store_sd(&ret, _mm_and_pd(_mm_load_sd(&sign_mask.__uf), _mm_load_sd(&x)));
+	return ret;
+#else
 	uint32_t high;
 	GET_HIGH_WORD(high,x);
 	SET_HIGH_WORD(x,high&0x7fffffff);
         return x;
+#endif
 }
